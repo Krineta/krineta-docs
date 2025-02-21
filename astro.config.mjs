0f51affd1f1643f49f2107330e31cc54b26774d7
dovahkiin0424 <dovahkiin0424@gmail.com>
2025-02-21 23:05:57 +0100

    Github link fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,7 +22,7 @@ export default defineConfig({
 				ThemeSelect: './src/components/ThemeComponent.astro',
 				},
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/FXTELEKOM',
 				discord: 'https://discord.gg/n2WmGaEn3H',
 			},
 			locales: {
