105800d1b4529a6968b9a09ace9800fd874f94e6
dovahkiin0424 <dovahkiin0424@gmail.com>
2025-05-28 02:02:49 +0200

    new fx docs theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,67 +2,73 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Krineta Docs',
-			favicon: './src/assets/krineta.jpg',
-			expressiveCode: {
-				themes: ['ayu-dark'],
-				},
-			head: [
-				{
-				tag: 'script',
-				attrs: {
-					src: 'https://a.fxtelekom.hu/script.js',
-					'data-website-id': '0c95ea3c-6388-460f-a15a-7269b9b0f285',
-					defer: true,
-				  },
-				},
-			  ],
-			logo: {
-				src: './src/assets/krineta.jpg',
-				replacesTitle: true,
-			},
-		    customCss: [
-			  './src/styles/custom.css',
-				],
-			components: {
-				ThemeSelect: './src/components/ThemeComponent.astro',
-				ThemeProvider: './src/components/ThemeProvider.astro',
-				},
-			social: {
-				github: 'https://github.com/krineta',
-				discord: 'https://discord.gg/n2WmGaEn3H',
-			},
-			locales: {
-				root: {
-					label: 'Magyar',
-					lang: 'hu',
-				},
-				},
-			sidebar: [
-				{
-					label: 'Krineta VPN',
-					items: [
-						'vpn/info',
-						'vpn/dtag',
-						'vpn/services',
-						{
-							label: 'Első lépések',
-							items: [
-								'vpn/first-steps/config',
-								'vpn/first-steps/install',
-								'vpn/first-steps/setup',
-							],
-						},
-						'vpn/router',
-						'vpn/troubleshoting',
-						'vpn/support',
-					],
-				},
-			],
-		}),
+  integrations: [
+      starlight({
+          title: 'Krineta Docs',
+          favicon: './src/assets/favicon.ico',
+          expressiveCode: {
+              themes: ['ayu-dark'],
+              },
+          head: [
+              {
+              tag: 'script',
+              attrs: {
+                  src: 'https://a.fxtelekom.hu/script.js',
+                  'data-website-id': '0c95ea3c-6388-460f-a15a-7269b9b0f285',
+                  defer: true,
+                },
+              },
+            ],
+          logo: {
+              src: './src/assets/favicon.jpg',
+              replacesTitle: true,
+          },
+          customCss: [
+            './src/styles/global.css',
+              ],
+          components: {
+              ThemeSelect: './src/components/ThemeComponent.astro',
+              ThemeProvider: './src/components/ThemeProvider.astro',
+              },
+          social: [
+             { icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' },
+             { icon: 'discord', label: 'Discord', href: 'https://astro.build/chat' },
+           ],
+          locales: {
+              root: {
+                  label: 'Magyar',
+                  lang: 'hu',
+              },
+              },
+          sidebar: [
+              {
+                  label: 'Krineta VPN',
+                  items: [
+                      'vpn/info',
+                      'vpn/dtag',
+                      'vpn/services',
+                      {
+                          label: 'Első lépések',
+                          items: [
+                              'vpn/first-steps/config',
+                              'vpn/first-steps/install',
+                              'vpn/first-steps/setup',
+                          ],
+                      },
+                      'vpn/router',
+                      'vpn/troubleshoting',
+                      'vpn/support',
+                  ],
+              },
+          ],
+      }),
 	],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
